acc5d357bc18bf631ac07327e9a0f9a30b6a1415
dimituri <none@none>
2010-08-02 19:37:37 +0300

    Put version information in a separate file

--- lib/tanuki.rb
+++ lib/tanuki.rb
@@ -2,7 +2,6 @@ module Tanuki
   libdir = File.dirname(__FILE__)
   $:.unshift(libdir) unless $:.include?(libdir)
   CLASSES_DIR = File.join(libdir, 'tanuki')
-  VERSION = 'alpha'
 end
 
 require 'rack'
@@ -10,6 +9,7 @@ require 'fileutils'
 require 'yaml'
 require 'escape_utils'
 require 'escape_utils/url/rack'
+require 'tanuki/version'
 require 'tanuki/configurator'
 require 'tanuki/context'
 require 'tanuki/launcher'
---
+++ lib/tanuki/version.rb
@@ -0,0 +1,3 @@
+module Tanuki
+  VERSION = 'alpha'
+end
