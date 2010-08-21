d9a33d87f7846da56d18c1bc1031c61ea086e4e4
dimituri <dimituri@gmail.com>
2010-08-21 03:00:11 +0300

    Meta model inheritance and method fixes

--- app/tanuki/meta_model/meta_model.rb
+++ app/tanuki/meta_model/meta_model.rb
@@ -1,4 +1,4 @@
-class Tanuki_MetaModel
+class Tanuki_MetaModel < Tanuki_Object
 
   def initialize(namespace, name, data)
     @namespace = namespace
@@ -9,10 +9,14 @@ class Tanuki_MetaModel
   def class_name_for(class_type)
     case class_type
     when :model then "#{@namespace}_Model_#{@name}"
-    when :model_base then "#{@namespace}_Model_#{@name}_Base"
+    when :model_base then "#{@namespace}_Model_#{@name}_#{@name}Base"
     when :manager then "#{@namespace}_Manager_#{@name}"
-    when :manager_base then "#{@namespace}_Manager_#{@name}_Base"
+    when :manager_base then "#{@namespace}_Manager_#{@name}Base"
     end
+  end
+
+  def key
+    []
   end
 
 end
