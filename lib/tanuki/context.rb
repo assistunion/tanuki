4825449321a18684c6dbcaaf40fa012cbc2f15d6
dimituri <dimituri@gmail.com>
2010-09-07 15:15:15 +0300

    Moved Context self methods to class << self

--- lib/tanuki/context.rb
+++ lib/tanuki/context.rb
@@ -7,41 +7,45 @@ module Tanuki
 
     @_defined = {}
 
-    # Creates and returns child context object.
-    # This object's superclass is going to be current context class.
-    def self.child
-      child = Class.new(self)
-      child.instance_variable_set(:@_defined, {})
-      child
-    end
+    class << self
 
-    # Allowes arbitary values to be assigned to context with a +key=+ method.
-    # A reader in context object class is created for each assigned value.
-    def self.method_missing(sym, arg=nil)
-      match = sym.to_s.match(/\A(?!(?:child|method_missing)=\Z)([^=]+)(=)?\Z/)
-      raise "`#{sym}' method cannot be called for Context and its descendants" unless match
-      defined = @_defined
-      class << self; self; end.instance_eval do
-        method_sym = match[1].to_sym
-        if defined.include? method_sym
-          undef_method method_sym
-        else
-          defined[method_sym] = nil
-        end
-        if arg.is_a? Proc
-          define_method(method_sym, &arg)
-        else
-          define_method(method_sym) { arg }
-        end
-        return arg
-      end if match[2]
-      super
-    end
+      # Creates and returns child context object.
+      # This object's superclass is going to be current context class.
+      def child
+        child = Class.new(self)
+        child.instance_variable_set(:@_defined, {})
+        child
+      end
 
-    # Disallow context instantiation
-    def self.new
-      raise "contexts cannot be instantiated"
-    end
+      # Allowes arbitary values to be assigned to context with a +key=+ method.
+      # A reader in context object class is created for each assigned value.
+      def method_missing(sym, arg=nil)
+        match = sym.to_s.match(/\A(?!(?:child|method_missing)=\Z)([^=]+)(=)?\Z/)
+        raise "`#{sym}' method cannot be called for Context and its descendants" unless match
+        defined = @_defined
+        class << self; self; end.instance_eval do
+          method_sym = match[1].to_sym
+          if defined.include? method_sym
+            undef_method method_sym
+          else
+            defined[method_sym] = nil
+          end
+          if arg.is_a? Proc
+            define_method(method_sym, &arg)
+          else
+            define_method(method_sym) { arg }
+          end
+          return arg
+        end if match[2]
+        super
+      end
+
+      # Disallow context instantiation
+      def new
+        raise "contexts cannot be instantiated"
+      end
+
+    end # end class << self
 
   end # end Context
 
