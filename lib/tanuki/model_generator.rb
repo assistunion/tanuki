789318e74362c5fb8baa122f9619417518a75353
dimituri <dimituri@gmail.com>
2011-02-08 16:27:22 +0200

    80 columns refactor of ModelGenerator

--- lib/tanuki/model_generator.rb
+++ lib/tanuki/model_generator.rb
@@ -3,14 +3,16 @@ module Tanuki
   # Tanuki::ModelGenerator is used for, well, generating models
   class ModelGenerator
 
-    # A collection of all model definitions represented as hash of hashes where
-    # the first nesting level represents namespaces
-    # and the second one contains named model bodies +Hash+.
+    # A collection of all model definitions represented as hash of hashes
+    # where the first nesting level represents namespaces
+    # and the second one contains named model bodies hash.
     attr_reader :models
 
-    # +Hash+ for collecting the template render-time info for models.
-    # Keys in this hash are full qualified names of models (+namespace_model_name+).
-    # Values are hashes in the form of +{:generated => [], :failed => [], :skipped => []}+
+    # Hash for collecting the template render-time info for models.
+    # Keys in this hash are full qualified names of models
+    # (+namespace_model_name+). Base models are marked with +(base)+ suffix.
+    # Values are hashes in the form of
+    # +{:generated => [], :failed => [], :skipped => []}+
     # where all subentries correspond to template generation statuses.
     attr_reader :tried
 
@@ -24,11 +26,15 @@ module Tanuki
     # Loads all models into memory from a given +schema_root+ and prepares
     # their own properties to be rendered in class templates.
     def read_models(schema_root)
-      Dir.entries(schema_root).reject {|path| path =~ /\A\.{1,2}\Z/ }.each do |namespace_path|
-        namespace_name = namespace_path.split('_').map {|s| s.capitalize }.join
+      paths = Dir.entries(schema_root).reject {|path| path =~ /\A\.{1,2}\Z/ }
+      paths.each do |namespace_path|
+        namespace_name = namespace_path.camelize
         namespace = @models[namespace_name] = {}
-        Dir.glob(File.join(schema_root, namespace_path, 'models', '*.yml')) do |file_path|
-          model_name = File.basename(file_path, '.yml').split('_').map {|s| s.capitalize }.join
+        schema_glob = "#{schema_root}/#{namespace_path}/#{models}/*.yml"
+        Dir.glob(schema_glob) do |file_path|
+
+          # Create meta model for namespace and model name from schema
+          model_name = File.basename(file_path, '.yml').camelize
           meta_model = namespace[model_name] = Tanuki::MetaModel.new(
             namespace_name,
             model_name,
@@ -36,14 +42,24 @@ module Tanuki
             @models
           )
           meta_model.process!
-          if @tried.include? namespace_model_name = "#{namespace_name}.#{model_name}"
-            next
-          else
-            @tried[namespace_model_name] = {:generated => [], :failed => [], :skipped => []}
-            @tried["#{namespace_model_name} (base)"] = {:generated => [], :failed => [], :skipped => []}
-          end
-        end
-      end
+          namespace_model_name = "#{namespace_name}.#{model_name}"
+
+          # TODO is this necessary?
+          next if @tried.include? namespace_model_name
+
+          # Register namespace and model name
+          @tried[namespace_model_name] = {
+            :generated => [],
+            :failed    => [],
+            :skipped   => []
+          }
+          @tried["#{namespace_model_name} (base)"] = {
+            :generated => [],
+            :failed    => [],
+            :skipped   => []
+          }
+        end # glob
+      end # each
     end
 
     # Generates all models that were read from a given +schema_root+.
@@ -57,13 +73,15 @@ module Tanuki
     # Renders a model class by applying +class_type+ template
     # to a given +meta_model+. Classes are splitted in two parts:
     # * user-extendable part (+base=false+), which resides in +ctx.app_root+,
-    # * other part with framework-specific code (+base=true+), which resides in +ctx.gen_root+.
+    # * other part with framework-specific code (+base=true+),
+    # which resides in +ctx.gen_root+.
     # +namespace_model_name+ is used as a label for error report collection
     # in Tanuki::ModelGenerator#tried hash.
     def generate_class(meta_model, namespace_model_name, class_type, base)
       class_name = meta_model.class_name_for class_type
       namespace_model_name += ' (base)' if base
-      path = Tanuki::Loader.class_path(class_name, base ? @ctx.gen_root : @ctx.app_root)
+      root = base ? @ctx.gen_root : @ctx.app_root
+      path = Tanuki::Loader.class_path(class_name, root)
       if base || !(File.exists? path)
         begin
           dirname = File.dirname(path)
@@ -88,9 +106,9 @@ module Tanuki
         namespace.each do |model_name, meta_model|
           namespace_model_name = "#{namespace_name}.#{model_name}"
           {
-            :model => false,
-            :model_base => true,
-            :manager => false,
+            :model        => false,
+            :model_base   => true,
+            :manager      => false,
             :manager_base => true
           }.each do |class_type, base|
             generate_class meta_model, namespace_model_name, class_type, base
@@ -99,8 +117,9 @@ module Tanuki
       end
     end
 
-    # Iterates over all loaded model definitions, giving them a chance to meet each other
-    # and make any cross-model assumptions (like names for foreign keys).
+    # Iterates over all loaded model definitions, giving them a chance
+    # to meet each other and make any cross-model assumptions
+    # (like names for foreign keys).
     def process_models
       @models.each do |namespace_name, namespace |
         namespace.each do |model_name, meta_model|
