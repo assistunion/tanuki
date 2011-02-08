b12c241c893dd64c6786afb13a18e15354671e08
dimituri <dimituri@gmail.com>
2011-02-08 09:27:54 +0200

    80 columns refactor of Argument

--- lib/tanuki/argument.rb
+++ lib/tanuki/argument.rb
@@ -5,7 +5,8 @@ require 'tanuki/argument/string'
 
 module Tanuki
 
-  # Tanuki::Argument contains basic classes and methods for controller arguments.
+  # Tanuki::Argument contains basic classes and methods for controller
+  # arguments.
   module Argument
 
     @assoc = {}
@@ -14,18 +15,22 @@ module Tanuki
 
       # Removes argument association for a given type class +klass+.
       def delete(klass)
-        @assoc.delete(klass)
+        @assoc.delete klass
       end
 
-      # Associates a given type class +klass+ with an argument class +arg_class+.
+      # Associates a given type class +klass+ with an argument class
+      # +arg_class+.
       def store(klass, arg_class)
-        warn "Tanuki::Argument::Base is not an ancestor of `#{arg_class}'" unless arg_class.ancestors.include? Argument::Base
+        unless arg_class.ancestors.include? Argument::Base
+          warn "Tanuki::Argument::Base is not an ancestor of `#{arg_class}'"
+        end
         @assoc[klass] = arg_class
       end
 
       alias_method :[], :store
 
-      # Converts a given type object +obj+ to an argument object with optional +args+.
+      # Converts a given type object +obj+ to an argument object with
+      # optional +args+.
       def to_argument(obj, *args)
         if @assoc.include?(klass = obj.class)
           @assoc[klass].new(obj, *args)
--- lib/tanuki/argument/integer_range.rb
+++ lib/tanuki/argument/integer_range.rb
@@ -1,10 +1,12 @@
 module Tanuki
   module Argument
 
-    # Tanuki::Argument::IntegerRange is a class for +Integer+ arguments with a certain value range.
+    # Tanuki::Argument::IntegerRange is a class for +Integer+ arguments with
+    # a certain value range.
     class IntegerRange < Integer
 
-      # Initializes the argument with a +default+ value and allowed value +range+.
+      # Initializes the argument with a +default+ value and allowed value
+      # +range+.
       def initialize(range, default=nil)
         super(default ? default : range.first)
         @range = range
